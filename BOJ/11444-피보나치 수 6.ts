bd61fafb2b45b181acd2adb56a14c309383d97ee
seokzin <seokzin95@gmail.com>
2021-12-19 03:53:00 +0900

    [BOJ/11444] 피보나치 수 6 (G3) (실패)

---
+++ BOJ/11444-피보나치 수 6.ts
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+const input: string[] = (
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin") : `1000`
+)
+  .toString()
+  .split("\n")
+  .map((x: string) => x.trim());
+
+const solution = (input: string[]): number => {
+  // JS는 함수를 최상단 배치하는 것이 맞을까?
+  const multi = (a: number[][], b: number[][]): number[][] => {
+    let mul = Array.from(Array(2), () => Array(2).fill(0));
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        for (let k = 0; k < 2; k++) {
+          mul[i][j] += a[i][k] * b[k][j];
+        }
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        mul[i][j] %= 1000000007;
+      }
+    }
+
+    return mul;
+  };
+
+  let n = Number(input[0]);
+
+  let mat: number[][] = [
+    [1, 1],
+    [1, 0],
+  ];
+  let res: number[][] = [
+    [1, 0],
+    [0, 1],
+  ];
+
+  while (n) {
+    if (n % 2 === 1) {
+      res = multi(res, mat);
+    }
+    mat = multi(mat, mat);
+
+    n = Math.floor(n / 2); // JS는 n//2가 없음
+  }
+
+  return res[1][0];
+};
+
+// 풀 수 없음 - JS는 최대 안전 정수값이 존재. 그 이상은 부동소수점으로 저장하여 오차 발생 (Number.MAX_SAFE_INTEGER = 9007199254740991)
+console.log(solution(input));
+
+export {};
