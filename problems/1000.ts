ec9fa268901630cb5d50221e675753db411808f6
seokzin <seokzin95@gmail.com>
2022-09-19 00:49:56 +0900

    feat: 문제 템플릿 생성

--- problems/1000.ts
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-const add = (a, b) => a + b;
-
-const [a, b] = input.map(Number);
-
-console.log(add(a, b));
---
+++ template.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// test cases
+const tc = `1 2`;
+
+// get inputs
+const inputs: string[] = (
+  process.platform === 'linux' ? fs.readFileSync('/dev/stdin') : tc
+)
+  .toString()
+  .trim()
+  .split('\n');
+
+// utils
+const add = (a: number, b: number) => a + b;
+
+// solution
+const solution = () => {
+  const [a, b] = inputs[0].split(' ').map(Number);
+
+  return add(a, b);
+};
+
+console.log(solution());
+
+// ignore redeclare error
+export {};
