8a151559891d88aa7b5c88b831218649f72d73f4
seokzin <seokzin95@gmail.com>
2022-09-19 01:12:40 +0900

    solve: 01001 / B5 / A-B

---
+++ problems/01001-A-B.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// test cases
+const tc = `3 2`;
+
+// get inputs
+const inputs: string[] = (
+  process.platform === 'linux' ? fs.readFileSync('/dev/stdin') : tc
+)
+  .toString()
+  .trim()
+  .split('\n');
+
+// utils
+const sub = (a: number, b: number) => a - b;
+
+// solution
+const solution = () => {
+  const [a, b] = inputs[0].split(' ').map(Number);
+
+  return sub(a, b);
+};
+
+console.log(solution());
+
+// ignore redeclare error
+export {};
