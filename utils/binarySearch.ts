a1ce43b76c2647252156a44baffdb477d8d98c3f
seokzin <seokzin95@gmail.com>
2022-09-21 08:54:14 +0900

    refac: 변수명 의미 명확하게 변경

--- utils/binarySearch.ts
+++ utils/binarySearch.ts
@@ -1,12 +1,12 @@
-export const binarySearch = (arr: number[], target: number) => {
+export const binarySearch = (ascendingArray: number[], target: number) => {
   let start = 0;
-  let end = arr.length - 1;
+  let end = ascendingArray.length - 1;
   let mid = Math.floor((start + end) / 2);
 
   while (start <= end) {
-    if (arr[mid] === target) {
+    if (ascendingArray[mid] === target) {
       return mid;
-    } else if (arr[mid] > target) {
+    } else if (ascendingArray[mid] > target) {
       end = mid - 1;
     } else {
       start = mid + 1;
