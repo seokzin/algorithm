4175409247e3373fbfcfca4d0cc4eb3cd6d4aa13
seokzin <seokzin95@gmail.com>
2022-10-01 22:57:05 +0900

    feat: 특수기호 제거 로직 추가

--- utils/isAnagram.ts
+++ utils/isAnagram.ts
@@ -1,5 +1,6 @@
 export const isAnagram = (str1: string, str2: string) => {
-  const strArray = (str: string) => str.toLowerCase().split('').sort().join('');
+  const strArray = (str: string) =>
+    str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 
   return strArray(str1) === strArray(str2);
 };
