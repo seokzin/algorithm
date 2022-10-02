5f19fe63ac1a204c58d6ee92f9b2d62a9dc04310
seokzin <seokzin95@gmail.com>
2022-10-02 20:48:11 +0900

    refac: 템플릿 변수명 간소화

--- problems/template.test.ts
+++ problems/template.test.ts
@@ -3,14 +3,14 @@ import { solution } from './template';
 
 describe('00000-template >', () => {
   it('tc #1 >', () => {
-    const tc1 = `1 2`;
-    const an1 = 3;
-    expect(solution(input(tc1))).toBe(an1);
+    const tc = `1 2`;
+    const an = 3;
+    expect(solution(input(tc))).toBe(an);
   });
 
   it('tc #2 >', () => {
-    const tc2 = `3 4`;
-    const an2 = 7;
-    expect(solution(input(tc2))).toBe(an2);
+    const tc = `3 4`;
+    const an = 7;
+    expect(solution(input(tc))).toBe(an);
   });
 });
