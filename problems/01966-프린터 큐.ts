4b98b4e14b9ac0577c80120a9236f7bb7880d0eb
seokzin <seokzin95@gmail.com>
2022-10-05 16:24:13 +0900

    solve: 01966 / 프린터 큐 / S3 (WIP)

--- problems/01966-프린터 큐.ts
+++ problems/01966-프린터 큐.ts
@@ -8,21 +8,25 @@ const input = (tc = '\n'.repeat(100)) =>
 
 // utils
 const getDocument = (priorities: number[], target: number) => {
-  // const shiftBack = (obj: [key : number] : number) => {
-  //   if (obj[0] < Math.max(Object.values(obj))) {
-  //     return obj;
-  //   }
-  // };
-
   const dic = priorities.reduce(
-    (acc, cur, idx) => ({
-      ...acc,
-      [idx]: cur,
-    }),
-    {}
+    (acc, cur, idx) => [...acc, { idx, priority: cur }],
+    []
   );
 
-  console.log(Object.values(dic));
+  dic.forEach(({ idx, priority }, i) => {
+    console.log(idx, priority, i, dic);
+    console.log('priority', priority, Math.max(...dic.map((d) => d.priority)));
+    if (priority < Math.max(...dic.map((d) => d.priority))) {
+      console.log('111');
+      dic.push(dic.shift());
+    } else if (idx === target) {
+      console.log('222');
+      return i + 1;
+    } else {
+      console.log('333');
+      dic.shift();
+    }
+  });
 };
 
 const range = (start: number, end?: number, step: number = 1): number[] => {
