96620d4968b2eb6b582a8a78ee543c91505d6a4f
seokzin <seokzin95@gmail.com>
2022-10-09 22:11:52 +0900

    feat: 딕셔너리 생성 로직 구현

---
+++ structures/dictionary.test.ts
@@ -0,0 +1,15 @@
+import { dictionary } from './dictionary';
+
+describe('dictionary >', () => {
+  it('keys & value', () => {
+    expect(dictionary(['a', 'b', 'c'], 1)).toEqual({
+      a: 1,
+      b: 1,
+      c: 1,
+    });
+  });
+
+  it('empty key & value', () => {
+    expect(dictionary([], 1)).toEqual({});
+  });
+});
