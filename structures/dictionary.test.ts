47f3b9dab3ed78fb227d54f2e5ca850ad343be98
seokzin <seokzin95@gmail.com>
2022-10-09 22:12:20 +0900

    test: fromEntries 예시 테스트 코드 추가

--- structures/dictionary.test.ts
+++ structures/dictionary.test.ts
@@ -12,4 +12,18 @@ describe('dictionary >', () => {
   it('empty key & value', () => {
     expect(dictionary([], 1)).toEqual({});
   });
+
+  it('Object.fromEntries() >', () => {
+    expect(
+      Object.fromEntries([
+        ['a', 1],
+        ['b', 1],
+        ['c', 1],
+      ])
+    ).toEqual({
+      a: 1,
+      b: 1,
+      c: 1,
+    });
+  });
 });
