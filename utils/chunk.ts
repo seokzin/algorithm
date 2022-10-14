7ab02fa0ac798533ece6157108d9fe20db7100d9
seokzin <seokzin95@gmail.com>
2022-10-14 14:50:54 +0900

    refac: chunk 함수에 제네릭 추가

--- utils/chunk.ts
+++ utils/chunk.ts
@@ -1,5 +1,5 @@
-export const chunk = (arr: any[], size: number) =>
+export const chunk = <T>(arr: T[], size: number) =>
   arr.reduce(
     (acc, _, i) => (i % size ? acc : [...acc, arr.slice(i, i + size)]),
-    []
+    [] as T[][]
   );
