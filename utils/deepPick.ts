c07934b750aa5ddeb6b0269863aaa7e92ff96d13
seokzin <seokzin95@gmail.com>
2022-10-21 10:31:19 +0900

    feat: deepPick 함수 구현

---
+++ utils/deepPick.ts
@@ -0,0 +1,7 @@
+export const deepPick = (keys: string, obj: Record<string, unknown>) => {
+  const [first, ...rest] = keys.split('.');
+
+  return rest.length
+    ? deepPick(rest.join('.'), obj[first] as Record<string, unknown>)
+    : obj[first];
+};
